79ae7c8cd7c977553f82f1bf078eeddc857f14be
Edward Vera <edwardismael1@gmail.com>
2023-09-20 17:39:40 -0300

    cambiosx3

--- client/src/components/cardProyect/CardProyect.jsx
+++ client/src/components/cardProyect/CardProyect.jsx
@@ -9,10 +9,10 @@ export default function CardProyect({props}){
         <div>
             <ImageCardProyect name={props.name} imageBg={props.imageBg} resume={props.resume}/>
             <div class=" bg-none w-[380px] h-[126px] sm:w-[420px] xl:w-[500px] xl:h-[350px]">
-                <div class="flex bg-gray-300 w-full h-[30px] md:h-[40px] text-center justify-center items-center">
+                <div class="flex bg-gray-300 w-full h-[24px] sm:h-[30px] md:h-[40px] text-center justify-center items-center">
                     <b class=" tracking-wider txt-lg mdMiddle:text-xl">{props.name}</b>
                 </div>
-                <div class=" bg-whitesmoke grid grid-col-[30%,70%] w-[380px] sm:w-[420px] h-[100px] xl:w-[500px] xl:h-[110px] rounded-bl-[10px] rounded-br-[10px]">
+                <div class=" bg-whitesmoke grid grid-col-[30%,70%] w-[380px] sm:w-[420px] h-[90px] sm:h-[100px] xl:w-[500px] xl:h-[110px] rounded-bl-[10px] rounded-br-[10px]">
                     <div class="flex flex-wrap text-center gap-x-2 justify-center items-center pt-2 h-[30px] mdh-[60px]">
                         {
                             props.arrayTechs.map((techIcon) => (
@@ -20,16 +20,16 @@ export default function CardProyect({props}){
                             ))
                         }
                     </div>
-                    <div class="flex justify-between h-[50px] pl-6 pr-6 items-center text-base xl:text-xl xl:pt-3">
+                    <div class="flex justify-between h-[50px] pl-6 pr-6 pt-2 sm:pt-0 items-center text-base xl:text-xl xl:pt-3">
                         <p>{props.date}</p>
                         <div class="flex">
                            <button class=" bg-sky-600 w-8 h-8 xl:w-10 xl:h-10 rounded-full flex items-center justify-center">
-                            <a href={props.pageURL}>
+                            <a href={props.pageURL} target="_blank">
                                 <img src={linkButton} class="w-[24px] h-[24px] xl:w-[30px] xl:h-[30px]" alt="Enlace" />
                             </a>
                             </button> 
                             <button class=" bg-sky-600 w-8 h-8 xl:w-10 xl:h-10 rounded-full flex items-center justify-center ml-4">
-                                <a href={props.githubURL}>
+                                <a href={props.githubURL} target="_blank">
                                     <img src={githubButton} class="w-[24px] h-[24px] xl:w-[30px] xl:h-[30px]" alt="Github" />
                                 </a>
                             </button>
