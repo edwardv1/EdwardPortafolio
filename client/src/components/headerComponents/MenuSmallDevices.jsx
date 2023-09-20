ddab8429b1c2e8fe84cd948b98460e692dce1447
Edward Vera <edwardismael1@gmail.com>
2023-09-20 01:39:34 -0300

    cambios en scroll smallDevice

--- client/src/components/headerComponents/MenuSmallDevices.jsx
+++ client/src/components/headerComponents/MenuSmallDevices.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import inicio from "../../assets/inicio.png";
 import about from "../../assets/about.png";
 import proyects from "../../assets/proyects.png";
@@ -8,14 +8,24 @@ import contact from "../../assets/contact.png";
 export default function MenuSmallDevices({navClass, menuVisible}){
 
     //Control del ScrollY al hacer click en las opciones
-    const scrollToSection = (scrollOffset) => {
-        window.scrollTo({ top: scrollOffset, behavior: 'smooth' });
-    };
+    // const scrollToSection = (scrollOffset) => {
+    //     window.scrollTo({ top: scrollOffset, behavior: 'smooth' });
+    // };
+
+    const [section, setSection] = useState(null);
+
+    const scrollToSection = (id) => {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+        setSection(id); // Actualizamos el estado para resaltar el enlace activo
+      }
+    }
 
     return(
         <div className={`md:hidden ${navClass} fixed top-[78px] right-0 h-screen w-[200px]  pl-4 transform transition-transform duration-1000 ease-out ${menuVisible ? 'translate-x-0 ' : 'translate-x-full'}`}>
             <ul class="flex h-5/6 flex-col items-center text-center justify-around w-full ">
-                <li class="  w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection(0)}>
+                <li class="  w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection('Inicio')}>
                     <a href="#Inicio">
                         <b>Inicio</b>
                     </a>
@@ -23,7 +33,7 @@ export default function MenuSmallDevices({navClass, menuVisible}){
                         <img src={inicio} alt="Inicio" class="w-[14px]  object-cover" />
                     </div>
                 </li>
-                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection(800)}>
+                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection('AboutMe')}>
                     <a href="#AboutMe">
                         <b>Sobre mi</b>
                     </a>
@@ -31,7 +41,7 @@ export default function MenuSmallDevices({navClass, menuVisible}){
                         <img src={about} alt="Sobre mi" class="w-[11.5px]  object-cover" />
                     </div>
                 </li>
-                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection(1600)}>
+                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection('Skills')}>
                     <a href="#Skills">
                         <b>Habilidades</b>
                     </a>
@@ -39,15 +49,15 @@ export default function MenuSmallDevices({navClass, menuVisible}){
                         <img src={skills} alt="Habilidades" class="w-[16px]  object-cover" />
                     </div>
                 </li>
-                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection(2500)}>
+                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection('Proyectos')}>
                     <a href="#Proyectos">
-                        <b> Proyectos</b>
+                        <b>Proyectos</b>
                     </a>
                     <div class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 py-[10.8px] px-[32px]">
                         <img src={proyects} alt="Proyectos" class="w-[16px]  object-cover" />
                     </div>
                 </li>
-                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection(3350)}>
+                <li class=" w-full mr-[16px] border-t-2 border-b-2 border-white py-2 hover:border-sky-600 cursor-pointer transform transition-transform duration-500 hover:scale-105 text-white overflow-hidden group" onClick={() => scrollToSection('Contacto')}>
                     <a href="#Contacto">
                         <b>Contacto</b>
                     </a>
